f7dad460710568c332313b8ecfb89330d8f5a196
Sanjeev Kumar <sanjeku@microsoft.com>
2020-06-15 20:08:38 +0200

    Added terraform steps to script

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -10,4 +10,3 @@ ARM_TENANT_ID=$4
 echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}" >> azurecreds.sh
 source ./azurecreds.sh
 cd souvenir/scenarios/hana-single-node-full
-terraform plan > plan.txt
