d64a86479fd2d43c09e8a8dbf62ce78daab14d37
Sanjeev Kumar <sanjeku@microsoft.com>
2020-06-16 18:54:28 +0200

    Fixed domain issue with HANA VM

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -10,6 +10,8 @@ ARM_TENANT_ID=$4
 echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=TRACE" >> azurecreds.sh
 $(cat ./azurecreds.sh)
 cd souvenir/scenarios/hana-single-node-full
+rm terraform.tfvars
+cp ./dev/terraform/terraform.tfvars .
 git pull
 sudo su - sanjeevkumar761
 terraform apply -var net_rg_name=$5 -var az_resource_group=$5 -auto-approve
