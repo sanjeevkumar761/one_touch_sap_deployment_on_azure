cc00a478095d044ee5dedde9d78e09b66ff081da
Sanjeev Kumar <sanjeku@microsoft.com>
2020-06-17 12:06:56 +0200

    Added log streaming to browser

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -7,7 +7,7 @@ ARM_SUBSCRIPTION_ID=$1
 ARM_CLIENT_ID=$2
 ARM_CLIENT_SECRET=$3
 ARM_TENANT_ID=$4
-echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=TRACE" >> azurecreds.sh
+echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=DEBUG\nexport TF_LOG_PATH=/home/juser/terraform.log" >> azurecreds.sh
 $(cat ./azurecreds.sh)
 cd souvenir/scenarios/hana-single-node-full
 git pull
@@ -15,5 +15,6 @@ rm terraform.tfvars
 cp ./dev/terraform/terraform.tfvars .
 terraform apply -var net_rg_name=$5 -var az_resource_group=$5 -auto-approve &
 ls
-ansible-playbook hana-single-node-full.yml --extra-vars 10.0.0.6 &
+cd ./dev/ansible
+ansible-playbook hana-single-node-full.yml --extra-vars "variable_host=10.0.0.6" &
 ls
