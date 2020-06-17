ff05670e02b293294d2bb1a3e95855d3486ec5d2
Sanjeev Kumar <sanjeku@microsoft.com>
2020-06-17 13:54:15 +0200

    Changed Terraform log to Info

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -7,7 +7,7 @@ ARM_SUBSCRIPTION_ID=$1
 ARM_CLIENT_ID=$2
 ARM_CLIENT_SECRET=$3
 ARM_TENANT_ID=$4
-echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=DEBUG\nexport TF_LOG_PATH=/home/juser/terraform.log" >> azurecreds.sh
+echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=INFO\nexport TF_LOG_PATH=/home/juser/terraform.log" >> azurecreds.sh
 $(cat ./azurecreds.sh)
 cd souvenir/scenarios/hana-single-node-full
 frontail /home/juser/terraform.log /home/juser/ansible.log -d
