384eeb4acfe8add83405508a0412226a8ce16997
Sanjeev Kumar <sanjeku@microsoft.com>
2020-06-17 12:31:50 +0200

    Added log streaming to browser

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -10,6 +10,7 @@ ARM_TENANT_ID=$4
 echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=DEBUG\nexport TF_LOG_PATH=/home/juser/terraform.log" >> azurecreds.sh
 $(cat ./azurecreds.sh)
 cd souvenir/scenarios/hana-single-node-full
+frontail /home/juser/terraform.log /home/juser/ansible.log -d
 git pull
 rm terraform.tfvars
 cp ./dev/terraform/terraform.tfvars .
