ece95ae074d629a0b4f288213c58d64f39d86002
Sanjeev Kumar <sanjeku@microsoft.com>
2020-06-18 22:15:07 +0200

    Updated script

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -26,9 +26,9 @@ ARM_CLIENT_SECRET=$3
 ARM_TENANT_ID=$4
 echo "export AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID}\nexport AZURE_CLIENT_ID=${AZURE_CLIENT_ID}\nexport AZURE_SECRET=${AZURE_SECRET}\nexport AZURE_TENANT=${AZURE_TENANT}\nexport ARM_SUBSCRIPTION_ID=${ARM_SUBSCRIPTION_ID}\nexport ARM_CLIENT_ID=${ARM_CLIENT_ID}\nexport ARM_CLIENT_SECRET=${ARM_CLIENT_SECRET}\nexport ARM_TENANT_ID=${ARM_TENANT_ID}\nexport TF_LOG=TRACE\nexport TF_LOG_PATH=/home/juser/terraform.log" >> azurecreds.sh
 $(cat ./azurecreds.sh)
+git clone https://sanjeku@dev.azure.com/sanjeku/sap-infra-devops/_git/souvenir
 cd souvenir/scenarios/hana-single-node-full
 frontail /home/juser/terraform.log /home/juser/ansible.log -d
-git pull
 rm -f terraform.tfvars
 cp ./dev/terraform/terraform.tfvars .
 terraform apply -var net_rg_name=$5 -var az_resource_group=$5 -auto-approve
