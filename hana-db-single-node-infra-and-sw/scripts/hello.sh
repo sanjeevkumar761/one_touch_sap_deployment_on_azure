311981bfef716da1ade6ab6fc4cf6da639f0ab36
Sanjeev Kumar <sanjeevkumar761@gmail.com>
2020-06-27 22:57:24 +0200

    Update hello.sh

--- hana-db-single-node-infra-and-sw/scripts/hello.sh
+++ hana-db-single-node-infra-and-sw/scripts/hello.sh
@@ -5,7 +5,8 @@ npm i frontail -g
 frontail -d /home/juser/terraform.log /home/juser/ansible.log
 npm install /home/juser/souvenir/nodejs/
 node /home/juser/souvenir/nodejs/index.js
-echo '[hanadb] "10.0.0.6"' >> /etc/ansible/hosts
+echo '[hanadb]' >> /etc/ansible/hosts
+echo '10.0.0.6' >> /etc/ansible/hosts
 cat /dev/zero | ssh-keygen -q -N "" > /dev/null
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash 
 apt-get update
