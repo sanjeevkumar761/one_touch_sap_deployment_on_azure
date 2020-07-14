18147c414915f0512808f5384a0308ba1f4e94dc
Sanjeev Kumar <sanjeku@microsoft.com>
2020-07-14 19:09:21 +0200

    Added bastion deployment

--- s4hana-infra-and-sw-e/scripts/hello.sh
+++ s4hana-infra-and-sw-e/scripts/hello.sh
@@ -53,12 +53,12 @@ sudo rm /var/lib/dpkg/lock*
 apt-get install axel
 axel https://softsap.blob.core.windows.net/sapsoft/IMDB_SERVER20_047_0-80002031.SAR
 az login --service-principal --username $2 --password $3 --tenant $4
-if [ "$9" == "yes" ]
-then
+#if [ "$9" == "yes" ]
+#then
     az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/sanjeevkumar761/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/azure-bastion-deploy.json" --parameters location=$6 resourceGroup=$6 bastionHostName='s4bastion' subnetName='AzureBastionSubnet' publicIpAddressName='s4bastionpublicip' existingVNETName=$7 subnetAddressPrefix='10.0.1.0/27'
-else
-    echo "Asked not to create bastion"
-fi
+#else
+#    echo "Asked not to create bastion"
+#fi
 az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/sanjeevkumar761/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/jumpbox-windows-deploy.json" --parameters adminUsername='juser' adminPassword='Hana@123456' windowsOSVersion='2016-Datacenter' vmSize='Standard_D2_v3' location=$6 vNetName=$7 subnetName=$8
 cd souvenir/scenarios/hana-single-node-full-e/dev/terraform
 terraform init
