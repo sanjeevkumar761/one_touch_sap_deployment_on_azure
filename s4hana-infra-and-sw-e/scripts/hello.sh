72cece6115c9051230456a1148cf3e9e8069eb2b
Sanjeev Kumar <sanjeku@microsoft.com>
2020-07-16 21:21:44 +0200

    Added bastion address space

--- s4hana-infra-and-sw-e/scripts/hello.sh
+++ s4hana-infra-and-sw-e/scripts/hello.sh
@@ -55,7 +55,7 @@ axel https://softsap.blob.core.windows.net/sapsoft/IMDB_SERVER20_047_0-80002031.
 az login --service-principal --username $2 --password $3 --tenant $4
 #if [ "$9" == "yes" ]
 #then
-az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/sanjeevkumar761/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/azure-bastion-deploy.json" --parameters location=$6 bastionHostName='s4bastion' subnetName='AzureBastionSubnet' publicIpAddressName='s4bastionpublicip' existingVNETName=$7 subnetAddressPrefix='10.0.1.0/27'
+az deployment group create  --resource-group $5 --template-uri "https://raw.githubusercontent.com/sanjeevkumar761/one_touch_sap_deployment_on_azure/master/s4hana-infra-and-sw-e/azure-bastion-deploy.json" --parameters location=$6 bastionHostName='s4bastion' subnetName='AzureBastionSubnet' publicIpAddressName='s4bastionpublicip' existingVNETName=$7 subnetAddressPrefix=$13
 #else
 #    echo "Asked not to create bastion"
 #fi
